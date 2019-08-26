d1e5b6bb7c7032140ee2903974ada6764040f64c
Emin Can Sumer <emin@emakina.nl>
2019-08-26 16:34:51 +0200

    Remove start option from mule command

--- resources/mule-start.sh
+++ resources/mule-start.sh
@@ -4,4 +4,4 @@ set -e
 printf "Starting Mule..."
 
 # Start mule
-${MULE_HOME}/bin/mule start -M-Dspring.profiles.active=${MULE_ENV} -M-DMULE_ENV=${MULE_ENV} --M-Dmule.env=${MULE_ENV} -M-Dmule.verbose.exceptions=true -M-Dmule.config.path=$MULE_HOME/conf/
+${MULE_HOME}/bin/mule -M-Dspring.profiles.active=${MULE_ENV} -M-DMULE_ENV=${MULE_ENV} --M-Dmule.env=${MULE_ENV} -M-Dmule.verbose.exceptions=true -M-Dmule.config.path=$MULE_HOME/conf/
