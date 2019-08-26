2ffa94266bcc41ad0346adb8e140a275c366a102
Emin Can Sumer <emin@emakina.nl>
2019-08-26 13:06:16 +0200

    Revert "Initial commit"
    This reverts commit 2cc0a43767d461a13a4728151bc3872c33808922.

--- resources/mule-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-printf "Starting Mule..."
-
-# Start mule
-${MULE_HOME}/bin/mule start -M-Dspring.profiles.active=${MULE_ENV} -M-DMULE_ENV=${MULE_ENV} --M-Dmule.env=${MULE_ENV} -M-Dmule.verbose.exceptions=true -M-Dmule.config.path=$MULE_HOME/conf/
